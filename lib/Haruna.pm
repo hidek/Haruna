4b976f2bbada62f76dc4893eaa64537748ef4cec
Hideo Kimura <hide@hide-k.net>
2012-10-24 13:40:36 +0900

    initial commit

---
+++ lib/Haruna.pm
@@ -0,0 +1,32 @@
+package Haruna;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Haruna -
+
+=head1 SYNOPSIS
+
+  use Haruna;
+
+=head1 DESCRIPTION
+
+Haruna is
+
+=head1 AUTHOR
+
+Hideo Kimura E<lt>hide@hide-k.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
